0edf110b114c908294a3ccd667b8ba7706b8c22a
masum <masum.r@antino.io>
2023-07-18 03:04:28 +0530

    add linkedin

--- src/app/doc/page.jsx
+++ src/app/doc/page.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 export default function page() {
   return (
     <>  
       <div class="container mx-auto   py-4  text-4xl text-center">
+          {/* <p>Linkedin<LinkedInIcon/> </p> */}
+          {/* <LinkedInIcon/> */}
+          <a href="https://www.linkedin.com/in/masum-reza-148397199/">My Linkedin </a>
+          {/* <LinkedInIcon/> */}
           <p className='py-28 font-bold'>I'm using this technology</p>
         <ul>
         <b >Ui create</b>
